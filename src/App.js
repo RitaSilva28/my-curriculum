fe277280bdcdaf98215f80beef7ba6ff347c29ce
Rita Silva <rita.cfs28@gmail.com>
2021-11-17 17:38:24 +0000

    React set up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Intro from"./Intro"
+
+export default function App() {
+  return (
+    <div className="App">
+     <Intro/>
+    </div>
+  );
+}
+
---
+++ src/Intro.js
@@ -0,0 +1,12 @@
+import React from "react"
+import"./Intro.css"
+
+export default function Intro(){
+
+    return(
+        <div className="intro">
+            <h1>Rita Silva</h1>
+            <h4>My Portfolio</h4>
+        </div>
+    )
+}
