fc98d13afb35cc27b5f173ff4a0d64f823e1b056
Rita Silva <rita.cfs28@gmail.com>
2021-12-29 18:41:20 +0000

    edited contact description

--- src/Components/js/Contact.js
+++ src/Components/js/Contact.js
@@ -9,7 +9,7 @@ export default function Contact(){
     return ( <div  id="contact">
          <h1> <span className="title">Let's get in touch!<img class="trace" src={Trace2} alt="underline draw"/></span></h1>
 
-         <p className="contact-description">I'm currently open to work so if we think we can collaborate, you can follow me and send me a message in any of my social accounts or you can click the button bellow and send me an email! </p>
+         <p className="contact-description">I'm currently open to work so if you think we can collaborate, you can follow me and send me a message in any of my social accounts or you can click the button bellow and send me an email! 😃 </p>
 
          <button><a href="mailto:rita.cfs28@gmail.com">EMAIL ME</a></button>
       
