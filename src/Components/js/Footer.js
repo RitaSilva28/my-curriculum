e8978fe83d4fded46cfaa00f136aeb7a6afd5e22
Rita Silva <rita.cfs28@gmail.com>
2021-12-30 22:44:54 +0000

    added code source to footer

--- src/Components/js/Footer.js
+++ src/Components/js/Footer.js
@@ -6,6 +6,7 @@ export default function Footer(){
     return(
         <div className="footer">
             <p>Designed and built by Rita Silva</p>
+            <p>Source code <a href="https://github.com/RitaSilva28/my-curriculum" target={"_blank"} rel="noreferrer">here</a></p>
         </div>
     )
 
